f493a9b987a7df75cada4e487253f6e846ec0e2a
HuyMaster <huymastertx88@gmail.com>
2025-05-21 10:01:46 +0700

    Adds constructor parameters to Student class.

--- APDP_Console/Lab3/Student.cs
+++ APDP_Console/Lab3/Student.cs
@@ -1,10 +1,20 @@
 namespace APDP.Lab3;
 
-public class Student(string firstName, string lastName, DateTime? dateOfBirth = null)
+public class Student
 {
-    public int StudentID { get; set; }
-    public string FirstName { get; set; } = firstName;
-    public string LastName { get; set; } = lastName;
-    public DateTime? DateOfBirth { get; set; } = dateOfBirth;
-    public DateTime EnrollmentDate { get; set; } = DateTime.Now;
+    private static int _studentID;
+
+    public Student(string firstName, string lastName, DateTime? dateOfBirth = null)
+    {
+        _studentID += 1;
+        FirstName = firstName;
+        LastName = lastName;
+        DateOfBirth = dateOfBirth;
+    }
+
+    public int StudentID { get; private set; } = _studentID;
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public DateTime? DateOfBirth { get; private set; }
+    public DateTime EnrollmentDate { get; private set; } = DateTime.Now;
 }
