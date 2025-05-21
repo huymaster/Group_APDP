60c7eb17a8ebdaa6257796c0bdafff99025ba02d
HuyMaster <huymastertx88@gmail.com>
2025-05-21 07:50:17 +0700

    Adds LibraryItem.cs.

---
+++ APDP_Console/Lab2/LibraryItem.cs
@@ -0,0 +1,8 @@
+﻿namespace APDP.Lab2;
+
+public abstract class LibraryItem
+{
+    protected abstract string Title { get; set; }
+    protected abstract string Author { get; set; }
+    protected abstract DateTime PublicationDate { get; set; }
+}
