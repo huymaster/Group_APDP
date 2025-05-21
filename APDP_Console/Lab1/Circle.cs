339eef67fb72d4334602697e9bfe7e339d0c4491
HuyMaster <huymastertx88@gmail.com>
2025-05-21 08:10:01 +0700

    Rename lab1 to Lab1

--- APDP_Console/Lab1/Circle.cs
+++ APDP_Console/Lab1/Circle.cs
@@ -1,13 +1,15 @@
 public class Circle : Shape
 {
-    public double Radius { get; set; }
     public Circle(double radius)
     {
         Radius = radius;
     }
+
+    public double Radius { get; set; }
+
     // Implement the abstract method from the base class
     public override double Area()
     {
-        return Math.PI * Radius * Radius;}
+        return Math.PI * Radius * Radius;
+    }
 }
-    
--- APDP_Console/Lab1/Rectangle.cs
+++ APDP_Console/Lab1/Rectangle.cs
@@ -1,16 +1,18 @@
 public class Rectangle : Shape
 {
-    public double Length { get; set; }
-    public double Width { get; set; }
     public Rectangle(double length, double width)
     {
         Length = length;
         Width = width;
     }
+
+    public double Length { get; set; }
+
+    public double Width { get; set; }
+
     // Implement the abstract method from the base class
     public override double Area()
     {
         return Length * Width;
     }
 }
-
