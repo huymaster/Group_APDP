5a68ec36581eca2d1ea1187bedfb3b4df32d99c7
HuyMaster <huymastertx88@gmail.com>
2025-07-02 22:07:44 +0700

    Remove all exercise and lab files, simplifying the project.

---
+++ APDP_Console/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace APDP;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+    }
+}
--- WebApp/Controllers/FileController.cs
+++ WebApp/Controllers/FileController.cs
@@ -4,9 +4,4 @@ namespace WebApp.Controllers;
 
 public class FileController : Controller
 {
-    public IActionResult DownloadAPK()
-    {
-        var bytes = System.IO.File.ReadAllBytes("wwwroot/files/Android.apk");
-        return File(bytes, "application/vnd.android.package-archive", "CampusExpenseManager.apk");
-    }
 }
